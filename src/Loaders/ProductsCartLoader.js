5adaf9389eacb5cc29de4f7bdbab86ad72b6bcf9
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-04 11:21:13 +0600

    update data

--- src/Loaders/ProductsCartLoader.js
+++ src/Loaders/ProductsCartLoader.js
@@ -1,20 +1,20 @@
 import { storedData } from "../utilities/fakedb";
 
-export const productsCartLoader = async() => {
-    const res = await fetch("https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json");
-    const products = await res.json();
+export const productsCartLoader = async () => {
+  const res = await fetch("http://localhost:5000/products");
+  const products = await res.json();
 
-    const saveData = storedData()
+  const saveData = storedData();
 
-    let initialData = [];
-    for (const id in saveData) {
-        const findData = products.find(product => product.id === id);
-        if(findData){
-            const quantity = saveData[id]
-            findData.quantity = quantity;
-            initialData.push(findData)
-        }
+  let initialData = [];
+  for (const id in saveData) {
+    const findData = products.find((product) => product.id === id);
+    if (findData) {
+      const quantity = saveData[id];
+      findData.quantity = quantity;
+      initialData.push(findData);
     }
-    
-    return { products, initialData }
-}
+  }
+
+  return { products, initialData };
+};
